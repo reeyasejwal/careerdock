4be788bc418948d268ae5ec739281c10c0163365
Reeya Sejwal <reeyasejwal17@gmail.com>
2026-06-17 11:43:15 +0530

    Add Aiven MySQL cloud database connection

--- careerdock-backend/src/config/db.js
+++ careerdock-backend/src/config/db.js
@@ -1,12 +1,19 @@
 const mysql = require('mysql2/promise');
+const fs = require('fs');
+const path = require('path');
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  connectionLimit: 10,
+  ssl: {
+    ca: fs.readFileSync(path.join(__dirname, '../../ca.pem')),
+    rejectUnauthorized: true,
+  },
   waitForConnections: true,
+  connectionLimit: 10,
   dateStrings: true,
 });
 
