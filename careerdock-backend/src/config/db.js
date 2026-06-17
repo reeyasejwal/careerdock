5e4b23d413d183bab4234ff6e67c7c314fbd35da
Reeya Sejwal <reeyasejwal17@gmail.com>
2026-06-17 11:52:22 +0530

    Fix SSL config for Render deployment

--- careerdock-backend/src/config/db.js
+++ careerdock-backend/src/config/db.js
@@ -1,20 +1,20 @@
 const mysql = require('mysql2/promise');
-const fs = require('fs');
-const path = require('path');
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  port: parseInt(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  ssl: {
-    ca: fs.readFileSync(path.join(__dirname, '../../ca.pem')),
-    rejectUnauthorized: true,
+  ssl: process.env.DB_CA_CERT ? {
+    ca: process.env.DB_CA_CERT,
+    rejectUnauthorized: true
+  } : {
+    rejectUnauthorized: false
   },
   waitForConnections: true,
   connectionLimit: 10,
-  dateStrings: true,
+  queueLimit: 0
 });
 
 module.exports = pool;
